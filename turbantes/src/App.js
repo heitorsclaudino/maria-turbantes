e6030d3cb8f30a4830baaf5343f8daa21059cc33
Heitor <heitorsclaudino04@gmail.com>
2023-01-20 21:10:15 -0300

    updates

--- turbantes/src/App.js
+++ turbantes/src/App.js
@@ -4,17 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/home";
 import Produtos from './pages/nossosProdutos';
 import QuemSomos from './pages/quemSomos';
+import ProdutosContext from './contexts/produtos';
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' index element={ <Home /> } />
-        <Route path='produtos' element={ <Produtos /> } />
-        <Route path='quem-somos' element={ <QuemSomos /> } />
-      </Routes>
-    </BrowserRouter>
+    <ProdutosContext.Provider value={{ produtos : ['Turbante amarelo', 'Turbante vermelho'] }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' index element={ <Home /> } />
+          <Route path='produtos' element={ <Produtos /> } />
+          <Route path='quem-somos' element={ <QuemSomos /> } />
+        </Routes>
+      </BrowserRouter>
+    </ProdutosContext.Provider>
   );
 }
 
--- turbantes/src/pages/nossosProdutos/index.js
+++ turbantes/src/pages/nossosProdutos/index.js
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+
 import Header from "../../components/header";
 import Card from "../../components/productsCard";
+import ProdutosContext from "../../contexts/produtos";
 
 function Produtos(){
+    const { produtos } = useContext(ProdutosContext);
+    console.log(produtos) 
     return (
         <div className="container">
             <Header />
