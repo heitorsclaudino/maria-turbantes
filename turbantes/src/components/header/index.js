ffc0b3077966eaf40045cabe95545eb1eefec57b
heitorsclaudino <heitorsclaudino04@gmail.com>
2023-03-29 11:52:46 -0300

    eess

--- turbantes/src/components/header/index.js
+++ turbantes/src/components/header/index.js
@@ -5,7 +5,7 @@ function Header(){
     return (
         <nav className="navbar">
             <div className="marca">
-                <img src="https://images.pexels.com/photos/5901052/pexels-photo-5901052.jpeg?auto=compress&cs=tinysrgb&w=600" alt="mulher bonita"></img>
+                <img src="https://images.pexels.com/photos/5901052/pexels-photo-5901052.jpeg?auto=compress&cs=tinysrgb&w=600" alt="mulher bonita" className=""></img>
                 <h1 className="logo">Maria Turbantes</h1>
             </div>
             <ul className="list">
