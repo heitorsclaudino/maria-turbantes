6695536c4ba14d4f620676159741782e09779d92
heitorsclaudino <heitorsclaudino04@gmail.com>
2023-05-24 10:58:11 -0300

    commit

--- turbantes/src/components/header/index.js
+++ turbantes/src/components/header/index.js
@@ -3,9 +3,9 @@ import "../style.css";
 
 function Header(){
     return (
-        <nav className="navbar">
+        <nav className="bg-red-700">
             <div className="marca">
-                <img src="https://images.pexels.com/photos/5901052/pexels-photo-5901052.jpeg?auto=compress&cs=tinysrgb&w=600" alt="mulher bonita" className=""></img>
+                <img src="https://images.pexels.com/photos/5901052/pexels-photo-5901052.jpe?auto=compress&cs=tinysrgb&w=600" alt="mulher bonita" className=""></img>
                 <h1 className="logo">Maria Turbantes</h1>
             </div>
             <ul className="list">
